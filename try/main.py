828d674665a5e613f65de221db0886069f9169f6
Nikita <nikita.voloboev@gmail.com>
2024-06-22 05:15:21 +0400

    .

--- try/main.py
+++ try/main.py
@@ -1,3 +1,7 @@
+# TODO: https://claudette.answer.ai , https://x.com/jeremyphoward/status/1804150711028191557
+# try https://x.com/jeremyphoward/status/1804150720738005015
+# -
+
 def main():
     print(3 + 2)
 
